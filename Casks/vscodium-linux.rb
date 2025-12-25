60c902964addd574093a9968e303a17ef8f186af
BogdanD <bogdan.dinu@merchantpro.ro>
2025-12-25 15:08:12 +0000

    vscodium-linux 1.107.18627

--- Casks/vscodium-linux.rb
+++ Casks/vscodium-linux.rb
@@ -2,9 +2,9 @@ cask "vscodium-linux" do
   arch arm: "arm64", intel: "x64"
   os linux: "linux"
 
-  version "1.106.37943"
-  sha256 arm64_linux:  "6948f49ba9aa572654d0cc4f5974a7e58386e230dbcc87529197e4895369251e",
-         x86_64_linux: "3fbd5a7885626aa4a83297e0b4a3d902c725b1674122f33d0ef02863c726e4ec"
+  version "1.107.18627"
+  sha256 arm64_linux:  "df658fef820961520aba19ac322d0416a8d395600ee03802db8a6d9b706a4a98",
+         x86_64_linux: "82a07175de96c359c85e8be2c60b2e222bcb5e7d4ba195cde4d84ae1b2e64a78"
 
   url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium-linux-#{arch}-#{version}.tar.gz"
   name "VSCodium"
