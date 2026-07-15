6efca1605155d0a3d26927cda1c6757aa8126e8f
BogdanD <bogdan.dinu@merchantpro.ro>
2026-07-15 16:27:39 +0300

    feat(pi-coding-agent): ✨ add PiCodingAgent formula
    - Introduced a new formula for `PiCodingAgent`, an AI agent toolkit.
    - Includes installation steps and test cases to verify functionality.

---
+++ Formula/pi-coding-agent.rb
@@ -0,0 +1,37 @@
+class PiCodingAgent < Formula
+  desc "AI agent toolkit"
+  homepage "https://pi.dev/"
+  url "https://registry.npmjs.org/@earendil-works/pi-coding-agent/-/pi-coding-agent-0.80.7.tgz"
+  sha256 "c010db218d57d561765651bf42ec13adc22736693880b46cf1919b9bfc260ab3"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    (bin/"pi").write_env_script libexec/"bin/pi", PI_SKIP_VERSION_CHECK: 1
+
+    node_modules = libexec/"lib/node_modules/@earendil-works/pi-coding-agent/node_modules/"
+    deuniversalize_machos node_modules/"@mariozechner/clipboard-darwin-universal/clipboard.darwin-universal.node"
+
+    arch = Hardware::CPU.arm? ? "arm64" : "x64"
+    os = OS.linux? ? "linux" : "darwin"
+    node_modules.glob("koffi/build/koffi/*").each do |dir|
+      basename = dir.basename.to_s
+      rm_r(dir) if basename != "#{os}_#{arch}"
+    end
+
+    node_modules.glob("@earendil-works/pi-tui/native/**/prebuilds/*").each do |dir|
+      basename = dir.basename.to_s
+      rm_r(dir) if basename != "#{os}-#{arch}"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pi --version 2>&1")
+
+    ENV["GEMINI_API_KEY"] = "invalid_key"
+    output = shell_output("#{bin}/pi -p 'foobar' 2>&1", 1)
+    assert_match "API key not valid", output
+  end
+end
